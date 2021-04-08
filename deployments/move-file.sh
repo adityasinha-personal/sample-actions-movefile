f8b418058280e0a603e5618dbbf406e35ba226fd
adityasinha26 <aditya.sinha@zomato.com>
2021-04-09 02:39:17 +0530

    almost final

--- deployments/move-file.sh
+++ deployments/move-file.sh
@@ -1,6 +1,7 @@
 #! /bin/sh
-landing_location=temp/local/
 file_name=sample-file.txt
+version=$(cat version | grep version_name | sed -E "s/^version_name=(.*)$/\1/")
+landing_location=temp/$version/
 mkdir -p $landing_location && cp $file_name $landing_location 
 git add $landing_location$file_name
-git commit -m "Sample file moved for previous release"
+git commit -m "Sample file moved for $version version"
