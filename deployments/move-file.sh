1fc1005bd4d1a7739d3f53d76df8f15931bd0b77
adityasinha-personal <59874185+adityasinha-personal@users.noreply.github.com>
2021-04-09 04:02:12 +0530

    Update move-file.sh

--- deployments/move-file.sh
+++ deployments/move-file.sh
@@ -2,6 +2,4 @@
 file_name=sample-file.txt
 version=$(cat version | grep version_name | sed -E "s/^version_name=(.*)$/\1/")
 landing_location=temp/$version/
-mkdir -p $landing_location && cp $file_name $landing_location 
-git add $landing_location$file_name
-git commit -m "Sample file moved for $version version"
+mkdir -p $landing_location && cp -r $file_name $landing_location 
